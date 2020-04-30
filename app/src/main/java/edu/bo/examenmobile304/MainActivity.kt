61d2f85010ccefa56d030b0a59fb6ba9013f33ed
Ronald Escobar <ronaldescobarj@gmail.com>
2020-04-30 19:11:33 -0400

    added progress bar

--- app/src/main/java/edu/bo/examenmobile304/MainActivity.kt
+++ app/src/main/java/edu/bo/examenmobile304/MainActivity.kt
@@ -2,11 +2,22 @@ package edu.bo.examenmobile304
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        save_user_button.setOnClickListener {
+            progress_bar.visibility = VISIBLE;
+            var r = Runnable {
+                progress_bar.visibility = GONE;
+            }
+            Handler().postDelayed(r, 2000)
+        }
     }
 }
